33ccc696f4908a7094cce32668616bd21ae40518
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-04-04 00:08:14 +0300

    feat: add burger in main and time pages

--- pages/main/burger.js
+++ pages/main/burger.js
@@ -1,7 +1,7 @@
 const btnBurger = document.querySelector('.mobile__arrow');
 const burger = document.querySelector('.nav__burger');
 // const blackLayer = document.querySelector('.black-layer')
-const closeBtn = document.querySelector(".nav__butger_close");
+const closeBtn = document.querySelector(".nav__burger_close");
 
 btnBurger.addEventListener('click', () => {
     burger.classList.add('nav__burger_active');
