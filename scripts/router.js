5f1a9a14557c2f2339fc52903e0794da623db7c6
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-04-22 18:13:58 +0300

    fix: change link

--- scripts/router.js
+++ scripts/router.js
@@ -37,7 +37,7 @@ class Router {
     }
     goToRoute(htmlName) {
         (function(scope) { 
-            let url = '/pages/' + htmlName,
+            let url = 'pages/' + htmlName,
                 xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState === 4 && this.status === 200) {
