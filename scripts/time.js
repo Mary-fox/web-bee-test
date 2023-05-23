411d56da83b58ab61cdaaec9316a0b474d8d88e6
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-05-23 18:40:47 +0300

    feat: change timer

--- scripts/time.js
+++ scripts/time.js
@@ -8,8 +8,9 @@ function updateTimer() {
   const seconds = Math.floor(((duration % 3600000) % 60000) / 1000); 
     
 
+  if (window.location.pathname === '/time') {
       document.getElementById('timer').textContent = `${hours}:${minutes}:${seconds}`; 
-  
+  }
 
   // Обновление таймера каждую секунду
   setTimeout(updateTimer, 1000);
