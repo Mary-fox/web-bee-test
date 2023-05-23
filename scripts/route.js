1c369c27c321dc2f29aeef85980ff7c5ed2f80ab
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-05-23 19:28:35 +0300

    feat: change router

--- scripts/route.js
+++ scripts/route.js
@@ -1,21 +1,14 @@
 class Route {
     constructor(name, htmlName, defaultRoute) {
-    try {
-        if(!name || !htmlName) {
-            throw 'error: name and htmlName params are mandatories';
+        if (!name || !htmlName) {
+            throw new Error('Name and URL path are mandatory parameters');
         }
         this.name = name;
         this.htmlName = htmlName;
         this.default = defaultRoute;
-    } catch (e) {
-        console.error(e);
-    } //маршрут, который содержит имя маршрута, имя HTML-файла и флаг
-}
+    }
 
-
-isActiveRoute(location) {
-    const lastIndex = location.lastIndexOf('/')
-    const currentPath = `/${location.slice(lastIndex + 1)}`;
-    return currentPath === this.name;
-}
+    isActiveRoute(location) {
+        return location.endsWith(this.name);
+    }
 }
--- scripts/router.js
+++ scripts/router.js
@@ -12,32 +12,48 @@ class Router {
     }
 
     init() {
+        const routes = this.routes;
         document.addEventListener('click', event => {
-            event.preventDefault();
-            const { target } = event;
-            if (!target.matches('li a')) return;
-            window.history.pushState({}, '', target.href);
-            this.hasChanged();
+          event.preventDefault();
+          const { target } = event;
+          if (!target.matches('li a')) return;
+          window.history.pushState({}, '', target.href);
+          this.hasChanged(routes);
         });
-        window.addEventListener('popstate', () => this.hasChanged());
+        window.addEventListener('popstate', () => this.hasChanged(routes));
         window.addEventListener('pushState', () => {
-            window.addEventListener('popstate', () => this.hasChanged());
+          window.addEventListener('popstate', () => this.hasChanged(routes));
         });
-        this.hasChanged();
-    }
-
-    hasChanged() {
+      
+        // Обработка события beforeunload
+        window.addEventListener('beforeunload', event => {
+          // Получаем текущий URL
+          const currentUrl = window.location.pathname;
+          const activeRoute = routes.find(route => route.isActiveRoute(currentUrl));
+          // Проверяем, нужно ли загрузить соответствующий HTML-файл для активного маршрута
+          if (activeRoute) {
+            event.preventDefault();
+            this.goToRoute(activeRoute.htmlName);
+          } else {
+            // Перенаправляем пользователя на стартовую страницу
+            window.location.replace('/');
+          }
+        });
+      
+        this.hasChanged(routes);
+      }
+      hasChanged(routes) {
         const location = window.location.pathname;
-        const route = this.routes.find(route => location.endsWith(route.name) || route.default);
+        const route = routes.find(route => location.endsWith(route.name) || route.default);
         if (route && route.isActiveRoute(location)) {
             this.goToRoute(route.htmlName);
         } else {
             console.log(`No active route found for path: ${location}`);
         }
     }
-
     goToRoute(htmlName) {
         const url = 'pages/' + htmlName;
+        
         fetch(url)
             .then(response => {
                 if (response.ok) {
@@ -60,70 +76,3 @@ class Router {
     }
 };
 
-// class Router {
-//     constructor(routes) {
-//         if (!routes) {
-//             throw new Error('Routes not found');
-//         }
-//         this.routes = routes;
-//         this.rootElem = document.getElementById('content');
-//         this.init();
-//     }
-
-//     init() {
-//         const routes = this.routes;
-//            (function(scope, routes) { 
-//                window.addEventListener('hashchange', function (e) {
-//                    scope.hasChanged(scope, routes);
-//                });
-//            })(this, routes);
-//            this.hasChanged(this, routes); //проверяем текущий маршрут
-//        }
-//     // hasChanged(scope, routes) {
-//     //     const pathname = window.location.pathname;
-//     //     if (pathname.length > 0) {
-//     //       for (let i = 0, length = routes.length; i < length; i++) {
-//     //         const route = routes[i];
-//     //         if(route.isActiveRoute(pathname)) {
-//     //           scope.goToRoute(route.htmlName);
-//     //         }
-//     //       }
-//     //     } else {
-//     //       for (let i = 0, length = routes.length; i < length; i++) {
-//     //         const route = routes[i];
-//     //         if(route.default) {
-//     //           scope.goToRoute(route.htmlName);
-//     //         }
-//     //       }
-//     //     }
-//     //   } // проверяем путь URL-адреса и сравниваем его со всеми маршрутами, чтобы определить, какой маршрут активен. Если ни один маршрут не соответствует текущему пути, то выбираем маршрут по умолчанию.
-//     hasChanged() {
-//         const route = this.routes.find(route => window.location.pathname.startsWith(route.name));
-        
-//         if (!route) {
-//           throw new Error('Route not found');
-//         }
-      
-//         if (this.currentRoute && this.currentRoute.name === route.name) {
-//           return false;
-//         }
-      
-//         this.currentRoute = route;
-//         return true;
-//       }
-// goToRoute(htmlName) {
-//     (function(scope) { 
-//       let url = 'pages' + htmlName,
-//           xhttp = new XMLHttpRequest();
-//       xhttp.onreadystatechange = function () {
-//         if (this.readyState === 4 && this.status === 200) {
-//           scope.rootElem.innerHTML = this.responseText;
-//         }
-//       }; //вставляем содержимое MLHttpRequest в корневой элемент страницы.
-//       if(htmlName === "map.html") setTimeout(() => {ymaps.ready(initMap)}, 300) //инициализация карты
-//       if(htmlName === "time.html") setTimeout(() => { updateTimer()}, 300) // загрузка таймера
-//       xhttp.open('GET', url, true);
-//       xhttp.send();
-//     })(this);
-//   }
-// };
